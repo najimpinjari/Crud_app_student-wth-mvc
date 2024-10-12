3930067e9b59383e7559d44ce039ef863e688e7d
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2024-10-12 17:18:40 +0530

    new crud

---
+++ webform_with_crud/Student.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Student.aspx.cs" Inherits="webform_with_crud.Student" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Student Management</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="text-align: center">
+            <h2>Student Management</h2>
+            
+            <label>Name:</label>
+            <asp:TextBox ID="textname" runat="server"></asp:TextBox>
+            <br /><br />
+            
+            <label>Gender:</label>
+            <asp:DropDownList ID="ddlgender" runat="server">
+                <asp:ListItem Text="Select Gender" Value=""></asp:ListItem>
+                <asp:ListItem Text="Male" Value="Male"></asp:ListItem>
+                <asp:ListItem Text="Female" Value="Female"></asp:ListItem>
+            </asp:DropDownList>
+            <br /><br />
+            
+            <label>Fees:</label>
+            <asp:TextBox ID="textfees" runat="server"></asp:TextBox>
+            <br /><br />
+            
+            <asp:Button ID="btnadd" runat="server" Text="Add Student" OnClick="BtnAdd_click" />
+            <br /><br />
+            
+            <asp:GridView ID="gvstudent" runat="server" AutoGenerateColumns="false" 
+                OnRowCommand="gvStudents_RowCommand" style="margin: auto; width: 60%;">
+                <Columns>
+                    <asp:BoundField DataField="Id" HeaderText="ID" />
+                    <asp:BoundField DataField="Name" HeaderText="Name" />
+                    <asp:BoundField DataField="Gender" HeaderText="Gender" />
+                    <asp:BoundField DataField="Fees" HeaderText="Fees" />
+                    <asp:ButtonField ButtonType="Button" CommandName="Edit" Text="Edit" />
+                    <asp:ButtonField ButtonType="Button" CommandName="Delete" Text="Delete" />
+                </Columns>
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ webform_with_crud/Student.aspx.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.UI.WebControls;
+
+namespace webform_with_crud
+{
+    public partial class Student : System.Web.UI.Page
+    {
+        private string connectionstring = ConfigurationManager.ConnectionStrings["Myconnectionstring"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindGrid();
+            }
+        }
+
+        protected void BtnAdd_click(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionstring))
+            {
+                using (SqlCommand cmd = new SqlCommand("INSERT INTO Students (Name, Gender, Fees) VALUES (@Name, @Gender, @Fees)", connection))
+                {
+                    cmd.Parameters.AddWithValue("@Name", textname.Text);
+                    cmd.Parameters.AddWithValue("@Gender", ddlgender.SelectedValue);
+                    cmd.Parameters.AddWithValue("@Fees", Convert.ToInt32(textfees.Text));
+
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+
+            BindGrid();
+            ClearFields();
+        }
+
+        private void ClearFields()
+        {
+            textname.Text = "";
+            ddlgender.SelectedIndex = 0;
+            textfees.Text = "";
+        }
+
+        protected void BindGrid()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionstring))
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM Students", connection))
+                {
+                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                    {
+                        DataTable dt = new DataTable();
+                        sda.Fill(dt);
+                        gvstudent.DataSource = dt;
+                        gvstudent.DataBind();
+                    }
+                }
+            }
+        }
+
+        protected void gvStudents_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            // Get the row index from the CommandArgument
+            int rowIndex = Convert.ToInt32(e.CommandArgument);
+            GridViewRow row = gvstudent.Rows[rowIndex];
+
+            // Retrieve the Student ID from the first cell (assuming ID is the first column)
+            int studentId = Convert.ToInt32(row.Cells[0].Text);
+
+            if (e.CommandName == "Edit")
+            {
+                EditStudent(studentId);
+            }
+            else if (e.CommandName == "Delete")
+            {
+                DeleteStudent(studentId);
+            }
+        }
+
+        protected void EditStudent(int studentId)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionstring))
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM Students WHERE Id = @Id", connection))
+                {
+                    cmd.Parameters.AddWithValue("@Id", studentId);
+                    connection.Open();
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        textname.Text = reader["Name"].ToString();
+                        ddlgender.SelectedValue = reader["Gender"].ToString();
+                        textfees.Text = reader["Fees"].ToString();
+                    }
+                    connection.Close();
+                }
+            }
+        }
+
+        private void DeleteStudent(int studentId)
+        {
+            using (SqlConnection con = new SqlConnection(connectionstring))
+            {
+                using (SqlCommand cmd = new SqlCommand("DELETE FROM Students WHERE Id = @Id", con))
+                {
+                    cmd.Parameters.AddWithValue("@Id", studentId);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+
+            BindGrid();
+        }
+    }
+}
